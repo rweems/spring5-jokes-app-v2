d5856ee233517a33a0fb331f6c0a6a74373d7fa2
Rickey Weems <weems505@gmail.com>
2022-10-10 17:56:56 -0400

    Create joke controller and index page

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String getJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
+}
